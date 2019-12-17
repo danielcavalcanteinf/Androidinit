1b9d2aecb3c62acd8fe32a7402c08b1bae7b4adb
daniel.cavalcante <daniel.cavalcante@maximasistemas.com.br>
2019-12-17 17:00:43 -0200

    initiate

---
+++ app/src/main/java/com/example/clientesapp/models/Cliente.kt
@@ -0,0 +1,20 @@
+package com.example.clientesapp.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "clientes")
+data class Cliente(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val codigo : Int,
+
+    @ColumnInfo(name = "nome")
+    val nome : String,
+
+    @ColumnInfo(name = "fone")
+    val telefone : String,
+
+    val endereco : String
+)
