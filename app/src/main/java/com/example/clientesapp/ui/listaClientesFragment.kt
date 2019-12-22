52b38dfe9715ffd41e98d5f2f9a39808bc454125
Daniel <daniel.almeida.cavalcante@gmail.com>
2019-12-21 21:21:53 -0300

    Add nav action
    navigation

--- app/src/main/java/com/example/clientesapp/ui/listaClientesFragment.kt
+++ app/src/main/java/com/example/clientesapp/ui/listaClientesFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import com.example.clientesapp.R
+import kotlinx.android.synthetic.main.fragment_lista_clientes.*
 
 class ListaClientesFragment : Fragment() {
 
@@ -21,7 +24,13 @@ class ListaClientesFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+
+        btnNovoCliente.setOnClickListener {
+            it.findNavController()
+                .navigate(R.id.action_listaClientesFragment_to_novoClienteFragment)
+        }
+        viewModel = ViewModelProviders.of(this).get(ListaClientesViewModel::class.java)
+
+
     }
-
-
 }
